180b1a8abf8fd57919411a35b648e058b0bc907f
dariomontani <dariomontani@gmail.com>
2022-03-01 19:16:25 +0100

    complete

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,10 +1,10 @@
 <?php
 
-use Faker\Generator as Faker;
-use Illuminate\Support\Str;
-
-use App\Model\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Model\Post;
+use App\User;
 
 class PostSeeder extends Seeder
 {
@@ -20,6 +20,7 @@ class PostSeeder extends Seeder
             $newPost->title = $faker->sentence(3, true);
             $newPost->content = $faker->paragraph(5, true);
             $newPost->slug = Str::slug($newPost->title . '-' . $i, '-');
+            $newPost->user_id = User::inRandomOrder()->first()->id;
             $newPost->save();
         }
     }
